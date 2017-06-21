68c81dc2dcd4243505a5804d71021de20df12893
Meenakshi_S04 <Meenakshi_S04@MYSHEC155503L.ad.infosys.com>
2017-06-21 21:29:04 +0530

    change test case

--- src/main/java/com/ms/calc/Calculater.java
+++ src/main/java/com/ms/calc/Calculater.java
@@ -12,11 +12,11 @@ public class Calculater {
 	}
 
 	public double doMul(double num1, double num2) {
-		return num1 - num2;
+		return num1 * num2;
 	}
 
 	public double doDiv(double num1, double num2) {
-		return num1 - num2;
+		return num1 / num2;
 	}
 
 }
